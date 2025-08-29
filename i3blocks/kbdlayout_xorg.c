b89b40634cf222a568bbc107765cca7c2e0c0d87
vulpes-vulpeos <40931454+vulpes-vulpeos@users.noreply.github.com>
2025-08-29 16:00:07 +0300

    made block clickable

--- i3blocks/kbdlayout_xorg.c
+++ i3blocks/kbdlayout_xorg.c
@@ -4,7 +4,10 @@
 #include <X11/extensions/XKBstr.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+enum mouse_buttons { DUMMY, BTNL, BTNM, BTNR, SCWLU, SCWLD, SCWLL, SCWLR, BTNF, BTNB };
 
 int valid_token(char *token){
     // check if token is 1 char long and is a number
@@ -27,12 +30,26 @@ int main() {
     // Get current layout index
     XkbStateRec state;
     if (XkbGetState(dpy, XkbUseCoreKbd, &state) != 0) { XCloseDisplay(dpy); return 1;};
+    // Do something if clicked
+    char* button = getenv("BLOCK_BUTTON");
+    if (button != NULL) {
+        switch (atoi(button)) {
+            case BTNL: // Switch to next layout
+                XkbLockGroup(dpy, XkbUseCoreKbd, ++state.group);
+                XkbGetState(dpy, XkbUseCoreKbd, &state);
+                break;
+            default: break;
+        };
+    };
     // Get keyboard description
     XkbDescPtr desc = XkbGetKeyboard(dpy, XkbSymbolsNameMask, XkbUseCoreKbd);
     if (!desc || !desc->names) { XCloseDisplay(dpy); return 1; };
     // Get symbols. Ex: pc_us(mac)_ru(mac)_2_inet(evdev)_terminate(ctrl_alt_bksp)
     char *symbols = XGetAtomName(dpy, desc->names->symbols);
     if (!symbols) { XkbFreeKeyboard(desc, XkbSymbolsNameMask, True); XCloseDisplay(dpy); return 1; };
+    // Get full name. Ex.: English (Macintosh)
+    //char *group = XGetAtomName(dpy, desc->names->groups[state.group]);
+    //if (!group) { XkbFreeKeyboard(desc, XkbSymbolsNameMask, True); XCloseDisplay(dpy); return 1; };
     // Get current layout name. Valid token index == state.group. Ex: us(mac)
     char *token = strtok(symbols, "+:_"), *layout = NULL;
     int vt_ind = 0;
