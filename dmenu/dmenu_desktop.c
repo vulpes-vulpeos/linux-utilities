cf68681f0a5633eabd87ae50e3eb9bb2a810b722
vulpes-vulpeos <40931454+vulpes-vulpeos@users.noreply.github.com>
2026-05-12 15:28:44 +0300

    Update dmenu_desktop.c

--- dmenu/dmenu_desktop.c
+++ dmenu/dmenu_desktop.c
@@ -5,8 +5,11 @@
 #include <strings.h>
 #include <unistd.h>
 
-#define TERM   "kitty"
-#define DM_PT  "Search:" // prompt to the left of the input field
+#define TERM   "st"
+#define DM_PT  "Start:"
+#define COLNUM 2
+#define ROWNUM 10
+#define POSITION "-z 700 -x 610 -b"
 
 #define MAX_NAME_LEN 128
 
@@ -18,16 +21,16 @@ typedef struct DFlist {
     } **dentries;
 } DFlist;
 
-int compare_app_name(const void *a, const void *b) {
-    const char *key = (const char *)a;
-    const struct Dentry *entry = *(const struct Dentry **)b;
-    return strcasecmp(key, entry->app_name);
-}
+static void launch_app(DFlist *df_list, char *response) {
+    struct Dentry **result = NULL;
+    for (int ctr = 0; ctr < df_list->dentr_ctr; ++ctr){ // bsearch() failes in some cases, replaced with linear
+        if (strcasecmp(df_list->dentries[ctr]->app_name, response) == 0 && !df_list->dentries[ctr]->hid) {
+            result = &df_list->dentries[ctr];
+            break;
+        };
+    };
 
-void launch_app(DFlist *dflist, char *response) {
-    struct Dentry **result = bsearch(response, dflist->dentries, dflist->dentr_ctr, sizeof(struct Dentry *), compare_app_name);
-
-    if (!result || (*result)->hid) { printf("[dmenu_desktop] dmenu response \"%s\" not found in app list.\n", response); return; };
+    if (!result) { printf("[dmenu_desktop] dmenu response \"%s\" not found in app list.\n", response); return; };
 
     if (fork() == 0) {
         if ((*result)->term) { execlp(TERM, TERM, "-e", "sh", "-c", (*result)->exec_path, NULL);
@@ -36,7 +39,7 @@ void launch_app(DFlist *dflist, char *response) {
     }
 }
 
-void exec_comm(char *dmenu_comm, char ***sel_apps, unsigned long *sel_apps_num){
+static void exec_dmenu_cmd(char *dmenu_comm, char ***sel_apps, unsigned long *sel_apps_num){
     // Open dmenu, read and execute selected app
     FILE *pipe = popen(dmenu_comm, "r");
     if (!pipe) { perror("[dmenu_desktop] Failed to open dmenu pipe"); return; };
@@ -55,13 +58,11 @@ void exec_comm(char *dmenu_comm, char ***sel_apps, unsigned long *sel_apps_num){
     pclose(pipe);
 }
 
-char* constr_comm (DFlist *df_list){
+static char* constr_dmenu_cmd (DFlist *df_list){
     // counting length of all visible app names
     unsigned long names_str_len = 0;
     for (int ctr = 0; ctr < df_list->dentr_ctr; ++ctr){
-        if (!df_list->dentries[ctr]->hid) {
-            names_str_len += strlen(df_list->dentries[ctr]->app_name) + 1;
-        };
+        if (!df_list->dentries[ctr]->hid) { names_str_len += strlen(df_list->dentries[ctr]->app_name) + 1; };
     };
     // combine names into one string using new line as separator
     char *names_str = calloc(names_str_len, sizeof(char));
@@ -80,13 +81,13 @@ char* constr_comm (DFlist *df_list){
     names_str[names_str_len-1] = '\0';
     // construct full command
     char *command;
-    asprintf(&command, "echo \"%s\" | dmenu -b -i -p \"%s\"", names_str, DM_PT);
+    asprintf(&command, "echo \"%s\" | dmenu %s -i -g %d -l %d -p \"%s\"", names_str, POSITION, COLNUM, ROWNUM, DM_PT);
     free(names_str);
 
     return command;
 }
 
-void free_df_list(DFlist *df_list){
+static void free_df_list(DFlist *df_list){
     for(int ctr = 0; ctr < df_list->dentr_ctr; ++ctr) {
         free(df_list->dentries[ctr]->df_path);
         free(df_list->dentries[ctr]->app_name);
@@ -96,7 +97,7 @@ void free_df_list(DFlist *df_list){
     free(df_list->dentries);
 }
 
-void process_pair (struct Dentry *a, struct Dentry *b){
+static void process_pair (struct Dentry *a, struct Dentry *b){
     // do nothing if names are different
     if (strcmp(a->app_name, b->app_name) != 0) { return; };
     // get size of dir path
@@ -111,11 +112,11 @@ void process_pair (struct Dentry *a, struct Dentry *b){
     int fl_ha = strncmp(a->df_path, "/home", 5);
     int fl_hb = strncmp(b->df_path, "/home", 5);
     // .desktop entries from user home directory have priority
-    if (fl_ha == 0) { ++b->hid; };
-    if (fl_hb == 0) { ++a->hid; };
+    if (fl_ha == 0) { b->hid = 1; };
+    if (fl_hb == 0) { a->hid = 1; };
 }
 
-int qsort_comp(const void *a, const void *b) {
+static int qsort_comp(const void *a, const void *b) {
     struct Dentry *d1 = *(struct Dentry **)a;
     struct Dentry *d2 = *(struct Dentry **)b;
 
@@ -130,7 +131,7 @@ int qsort_comp(const void *a, const void *b) {
     return cmp;
 }
 
-int parse_dfile (struct Dentry *dentry){
+static int parse_dfile (struct Dentry *dentry){
     FILE *file = fopen(dentry->df_path, "r");
     if (!file){ fprintf(stderr, "[dmenu_desktop] ERROR: %s: ", dentry->df_path); perror(""); return 1; };
 
@@ -140,58 +141,40 @@ int parse_dfile (struct Dentry *dentry){
     while (fgets(line, sizeof(line), file)) {
         // remove new line at the end of the line
         len = strlen(line);
-        if (len > 0 && line[len - 1] == '\n') {
-            line[len - 1] = '\0';
-        };
+        if (len > 0 && line[len - 1] == '\n') { line[len - 1] = '\0'; };
         // look for needed data
-        if (!fl_an && !strncmp(line, "Name=", 5)) { 
-            dentry->app_name = strdup(line + 5);
-            ++fl_an;
+        if (!fl_an && !strncmp(line, "Name=", 5)) { dentry->app_name = strdup(line + 5); ++fl_an;
         } else if (!fl_ep && !strncmp(line, "Exec=", 5)) {
             // remove argument placeholder
             char *percent = strrchr(line + 5, '%');
             if (percent) { *(percent - 1) = '\0'; };
             dentry->exec_path = strdup(line + 5);
             ++fl_ep;
-        } else if (!dentry->term && !strcmp(line, "Terminal=true")) {
-            dentry->term = 1;
-        } else if (!dentry->hid && (!strcmp(line, "Hidden=true") || !strcmp(line, "NoDisplay=true"))) {
-            dentry->hid = 1;
-        };
-
+        } else if (!dentry->term && !strcmp(line, "Terminal=true")) { dentry->term = 1;
+        } else if (!dentry->hid && (!strcmp(line, "Hidden=true") || !strcmp(line, "NoDisplay=true"))) { dentry->hid = 1; };
         // In case of hidden entry we care only about app name
-        if (fl_an && dentry->hid) {
-            break;
-        };
+        if (fl_an && dentry->hid) { break; };
     };
-
     // Hide entries without a name
-    if (!fl_an) {
-        dentry->app_name = strdup("N/A");
-        dentry->hid = 1;
-    };
+    if (!fl_an) { dentry->app_name = strdup("N/A"); dentry->hid = 1; };
     // or execution path
-    if (!fl_ep) {
-        dentry->exec_path = strdup("N/A");
-        dentry->hid = 1;
-    };
+    if (!fl_ep) { dentry->exec_path = strdup("N/A"); dentry->hid = 1; };
 
     fclose(file);
-
     return 0;
 }
 
-int filter(const struct dirent *entry) {
+static int filter(const struct dirent *entry) {
     const char *ext = strrchr(entry->d_name, '.');
     return ext && strcmp(ext, ".desktop") == 0;
 }
 
-int parse_paths(char **paths, size_t num_paths, DFlist *df_list) {
-    for (size_t i = 0; i < num_paths; ++i) {
+static int parse_paths(char **paths, size_t num_paths, DFlist *df_list) {
+    for (size_t ctr = 0; ctr < num_paths; ++ctr) {
         // get dirent list of .desktop files
         struct dirent **de_list;
-        int de_ctr = scandir(paths[i], &de_list, filter, NULL);
-        if (de_ctr < 0) { fprintf(stderr, "[dmenu_desktop] ERROR: %s: ", paths[i]); perror(""); return 1; };
+        int de_ctr = scandir(paths[ctr], &de_list, filter, NULL);
+        if (de_ctr < 0) { fprintf(stderr, "[dmenu_desktop] ERROR: %s: ", paths[ctr]); perror(""); return 1; };
         if (de_ctr == 0) { continue; };
         // resize dentries array to fit new entries
         struct Dentry **tptr = NULL;
@@ -199,16 +182,16 @@ int parse_paths(char **paths, size_t num_paths, DFlist *df_list) {
         if (!tptr) { perror("[dmenu_desktop] ERROR: realloc: "); return 1; };
         df_list->dentries = tptr;
         // fill .desktop entries data
-        for (int j = 0; j < de_ctr; ++j) {
+        for (int ctr2 = 0; ctr2 < de_ctr; ++ctr2) {
             df_list->dentries[df_list->dentr_ctr] = calloc(1, sizeof(struct Dentry));
             if (!df_list->dentries[df_list->dentr_ctr]) { perror("[dmenu_desktop] ERROR: calloc: "); return 1; };
             // fill full path to .desktop entry
-            asprintf(&df_list->dentries[df_list->dentr_ctr]->df_path, "%s/%s", paths[i], de_list[j]->d_name);
+            asprintf(&df_list->dentries[df_list->dentr_ctr]->df_path, "%s/%s", paths[ctr], de_list[ctr2]->d_name);
             // fill app name, execution path, hidden and terminal flags
             if (parse_dfile(df_list->dentries[df_list->dentr_ctr])) { return 1; };
 
             ++df_list->dentr_ctr;
-            free(de_list[j]);
+            free(de_list[ctr2]);
         };
         free(de_list);
     };
@@ -216,7 +199,7 @@ int parse_paths(char **paths, size_t num_paths, DFlist *df_list) {
     return 0;
 }
 
-void usage () {
+static void usage () {
     puts("Usage: dmenu_dektop <DIR>...\n");
     puts("Arguments:");
     puts("<DIR>...    One or more directories containing .desktop files.");
@@ -230,28 +213,21 @@ int main (int argc, char **argv) {
     // parse provided path(s)
     DFlist df_list = {0};
     if (parse_paths(&argv[1], argc-1, &df_list)){ return 0; };
-
     // sort .desktop entries
     qsort(df_list.dentries, df_list.dentr_ctr, sizeof(struct Dentry*), qsort_comp);
-
     // process .desktop entries to hide duplicates
     for (int ctr = 0; ctr < df_list.dentr_ctr-1; ++ctr){ process_pair(df_list.dentries[ctr], df_list.dentries[ctr+1]); };
-
     // form dmenu command
-    char *dmenu_comm = constr_comm(&df_list);
+    char *dmenu_comm = constr_dmenu_cmd(&df_list);
     if (!dmenu_comm) { free_df_list(&df_list); return 0; };
-
     // execute dmenu command
     char **sel_apps = NULL;
     unsigned long sel_apps_num = 0;
-    exec_comm(dmenu_comm, &sel_apps, &sel_apps_num);
+    exec_dmenu_cmd(dmenu_comm, &sel_apps, &sel_apps_num);
     free(dmenu_comm);
-    // launch app
+    // launch app(s)
     if (sel_apps_num > 0){
-        for (int i = 0; i < sel_apps_num; ++i){
-            launch_app(&df_list, sel_apps[i]);
-            free(sel_apps[i]);
-        };
+        for (int i = 0; i < sel_apps_num; ++i){ launch_app(&df_list, sel_apps[i]); free(sel_apps[i]); };
         free(sel_apps);
     };
 
